8044029391fd67f818b10d1c6f74f21dfd2f3a3f
Ilya Ovdin <iovdin@gmail.com>
2015-10-05 10:10:02 +0300

    validate question that comes from spreadsheet

--- admin.js
+++ admin.js
@@ -46,27 +46,45 @@ if(Meteor.isServer) {
         var qs = Assets.getText('questionset - type1you.csv');
         var lines = Papa.parse(qs).data;
         var i; 
+        var skill;
         for (i = 1; i < lines.length; i++) {
             var l =  lines[i];
-            qdata.type1you.push({_id: String(i), skill: l[0], text: l[1]});
+            skill = l[0].trim()
+            if(!skill2category[skill]) {
+                console.log("type1you: unknown skill", skill,  l.join(" "));
+                continue;
+            }
+            qdata.type1you.push({_id: String(i), skill: skill, text: l[1]});
         }
 
         qs = Assets.getText('questionset - type1others.csv');
         lines = Papa.parse(qs).data;
         for (i = 1; i < lines.length; i++) {
             var l =  lines[i];
-            qdata.type1others.push({_id: String(i), skill: l[0], text: l[1]});
+            skill = l[0].trim()
+
+            if(!skill2category[skill]) {
+                console.log("type1others, unknown skill", skill, l.join(" "));
+                continue;
+            }
+            qdata.type1others.push({_id: String(i), skill: skill, text: l[1]});
         }
         qs = Assets.getText('questionset - type3.csv');
         lines = Papa.parse(qs).data;
         var question;
         for (i = 1; i < lines.length; i++) {
             var l =  lines[i];
+            skill = l[2].trim()
+
+            if(skill && !skill2category[skill]) {
+                console.log("type3 unknown skill", skill, l.join(" "));
+                continue;
+            }
             if(l[0]) {
                 qdata.type3.push(question);
-                question = { text : l[0], answers : [ {_id: String(i), text: l[1], skill: l[2]} ] };
+                question = { text : l[0], answers : [ {_id: String(i), text: l[1], skill: skill} ] };
             } else if(question){
-                question.answers.push({_id: String(i), text: l[1], skill: l[2]});
+                question.answers.push({_id: String(i), text: l[1], skill: skill});
             } 
         }
         qdata.type3.push(question);
